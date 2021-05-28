b03e9d4d48a484cc3ac3edede7131a329aadda8e
Mrinal Sinha <themrinalsinha@gmail.com>
2021-05-28 23:47:37 +0530

    desktop setup script v0.1 added

--- README.md
+++ README.md
@@ -2,3 +2,36 @@
 Repo containing helper shell scripts to install all the necessary packages while setting up desktop or servers. It mostly contains tools/packages that i use on daily basis.
 
 ## Documentation
+
+- **`desktop/setup.sh`** Desktop setup script
+  - List of packages it installs
+    ```
+    git, zip, zsh,gcc,htop,make,unzip,libc-dev,mitmproxy,libffi-dev, terminator, python3-pip, python3-dev, dconf-editor, gnome-tweaks, libxml2-utils, build-essential, apt-transport-https, indicator-multiload, gnome-shell-extensions
+    ```
+  - Setting up the default terminal
+    ```shell
+    $ sudo update-alternatives --config x-terminal-emulator
+    ```
+  - Setting up custom `zsh` prompt
+    ```shell
+    $ wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+    ```
+    Enabling `zsh` shell as default
+    ```shell
+    $ chsh
+    $ /bin/zsh # enter in the prompt
+    ```
+  - Installs flat icon theme from [snwh](https://launchpad.net/~snwh/+archive/ubuntu/ppa?field.series_filter=bionic)
+  - Run docker without `sudo`
+    ```shell
+    $ sudo usermod -aG docker ${USER}
+    $ su - ${USER}
+    $ id -nG
+    $ sudo usermod -aG docker ${USER}
+    ```
+
+---
+### Support
+- [x] Debian Based Machine
+- [ ] Redhat Based Machine
+- [ ] Mac Based Machine
---
+++ desktop/setup.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+SYSTEM=$(uname -s)
+
+# defining color encoding
+tput clear
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+RESET=`tput sgr0`
+BOLD=`tput bold`
+
+
+# function to check last executed command status
+check_status() {
+    if [ $? -eq 0 ]; then
+        echo "✅ ${GREEN}${BOLD}Successful${RESET}"
+    else
+        echo "❌ ${RED}${BOLD}Failed${RESET}"
+    fi
+}
+
+
+if [[ "$SYSTEM" == "Linux" ]]; then
+
+    if [ -f /etc/lsb-release ]; then
+        echo -e "🐧 ${BOLD}Debian based machine${RESET}\n"
+
+        # package list
+        ESSENTIAL_PACKAGES=(
+            'git'
+            'zip'
+            'zsh'
+            'gcc'
+            'htop'
+            'make'
+            'unzip'
+            'libc-dev'
+            'mitmproxy'
+            'libffi-dev'
+            'terminator'
+            'python3-pip'
+            'python3-dev'
+            'dconf-editor'
+            'gnome-tweaks'
+            'libxml2-utils'
+            'build-essential'
+            'apt-transport-https'
+            'indicator-multiload'
+            'gnome-shell-extensions'
+        )
+
+        # sudo apt-get update && sudo apt-get upgrade -y
+        check_status
+
+        echo -e "\n🐣 ${BOLD}Initiating package installation${RESET}\n"
+        for pkg in "${ESSENTIAL_PACKAGES[@]}"; do
+            echo -e "\n💿 ${YELLOW}Installing - ${RESET} ${BOLD}$pkg${RESET}"
+            sudo apt-get install "$pkg" -y
+            check_status
+        done
+
+        echo -e "\n🐣 ${BOLD}Enabling minimize & maximize option${RESET}\n"
+        gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+        gsettings set org.gnome.desktop.wm.preferences action-double-click-titlebar 'toggle-maximize'
+        check_status
+
+        echo -e "\n🐣 ${BOLD}Setting up lazygit${RESET}\n"
+        if ! [ -x "$(command -v lazygit)" ]; then
+            sudo add-apt-repository ppa:lazygit-team/release -y
+            sudo apt-get update
+            sudo apt-get install lazygit -y
+            check_status
+        fi
+
+        echo -e "\n🌀 ${BOLD}Cleaning up the mess${RESET}\n"
+        sudo apt autoclean && sudo apt autoremove
+        check_status
+    fi
+
+    if [ -f /etc/redhat-release ]; then
+        echo -e "🎩 ${BOLD}Redhat based machine${RESET}\n"
+
+        # TODO: add package support for redhat ecosystem
+    fi
+
+fi
+
+
+if [[ "$SYSTEM" == "Darwin" ]]; then
+    echo -e "🖥 ${BOLD}Mac based machine${RESET}\n"
+
+    # checking if brew is install: MacOS package manager
+    if test ! $(which brew); then
+        echo -e "🏠 Installing brew package manager\n"
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        check_status
+    fi
+
+    # TODO: add package support for mac
+fi
